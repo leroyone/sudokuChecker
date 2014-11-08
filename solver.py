64beda5656d69db65e664d0b3ab296caf03309c7
Leroy <thealloriginal@hotmail.com>
2014-11-08 00:10:24 +0000

    Abstracted solver idea

---
+++ solver.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+a = []
+
+for line in range(1,10):
+    b = []
+    for column in range(1,10):
+        b.append(0)
+    a.append(b)
+
+###### maker #####
+
+a[0][6] = 5
+a[0][7] = 2
+a[1][3] = 5
+a[1][4] = 3
+a[2][1] = 3
+a[2][3] = 4
+a[2][4] = 7
+a[3][0] = 1
+a[3][1] = 4
+a[3][6] = 2
+a[4][2] = 7
+a[4][3] = 1
+a[4][5] = 5
+a[4][6] = 6
+a[5][2] = 2
+a[5][7] = 7
+a[5][8] = 5
+a[6][4] = 4
+a[6][5] = 3
+a[6][7] = 8
+a[7][4] = 6
+a[7][5] = 7
+a[8][1] = 6
+a[8][2] = 8
+
+##############
+
+for each in
+
+for each in a:
+    print each
+
+
+#### check if number not in column or row
+#### if not, add to list
+
+#### when complete, if len(list item) == 1, pop and insert into table
+#### remove number from all list items in row and column
+
+#### wash, rinse, repeat
+
+#### if list item == [], ignore
+
+#### list items in dict?
+#### list of list items?
